822247cb07d5b7d78a446cc404198dcbf0abab71
Jakob Runge <sicarius@g4t3.de>
2020-01-18 23:08:03 +0100

    add upload button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React, { FC, useState, useEffect } from "react";
+import React, { FC, useState, useEffect, useCallback } from "react";
 import Alert from "@material-ui/lab/Alert";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
 
 import TextMessageEditor from "./TextMessageEditor";
 
-import { getAvailability } from "./bluetooth";
-import { emptyTextMessage } from "./text";
+import { getAvailability, uploadMessages } from "./bluetooth";
+import { emptyTextMessage, toMessage } from "./text";
 
 const useHasBluetooth = (): boolean => {
   const [has, setHas] = useState(false);
@@ -19,15 +21,36 @@ const useHasBluetooth = (): boolean => {
 const App: FC = () => {
   const gotBluetooth = useHasBluetooth();
   const [textMessage, setTextMessage] = useState(emptyTextMessage());
+  const [uploading, setUploading] = useState(false);
+  const [uploadError, setUploadError] = useState<string | undefined>(undefined);
+
+  const doUpload = useCallback(async () => {
+    setUploading(true);
+    const uploadError = await uploadMessages([toMessage(textMessage)]);
+    setUploading(false);
+    setUploadError(uploadError);
+  }, [textMessage, setUploading, setUploadError]);
 
   if (!gotBluetooth) {
     return <Alert severity="error">Bluetooth is not supported</Alert>;
   }
 
   return (
-    <div>
+    <Container>
+      {uploadError && <Alert severity="warning">{uploadError}</Alert>}
       <TextMessageEditor message={textMessage} setMessage={setTextMessage} />
-    </div>
+      <Button
+        variant="contained"
+        color="primary"
+        disabled={uploading}
+        onClick={doUpload}
+      >
+        <span role="img" aria-label="rocket">
+          🚀
+        </span>
+        Upload!
+      </Button>
+    </Container>
   );
 };
 
